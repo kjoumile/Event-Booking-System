ab0cbc6cef5f273d56aa7b183d11f9743a273f4a
kjoumile <125726260+kjoumile@users.noreply.github.com>
2025-11-27 08:59:51 +0300

    add new urls

--- booking_system/events/urls.py
+++ booking_system/events/urls.py
@@ -2,12 +2,19 @@ from django.urls import  path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+
 router = DefaultRouter()
 router.register('categories', views.CategoryViewSet)
 router.register('venues', views.VenueViewSet)
 router.register('events', views.EventViewSet)
 router.register('seats', views.SeatViewSet)
 router.register('bookings', views.BookingViewSet)
+router.register('roles', views.RoleViewSet)
+router.register('user-roles', views.UserRoleViewSet)
+router.register('reviews', views.ReviewViewSet)
+router.register('payments', views.PaymentViewSet)
+router.register('notifications', views.NotificationViewSet)
+router.register('logs', views.LogViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
